44422ceb8c9d0405f43ff627a609a77e8a51122a
asumanwebdev <asumanwebdev@gmail.com>
2022-06-09 20:33:55 +0300

    fix issues

--- src/Components/Navigation/MobileNav.jsx
+++ src/Components/Navigation/MobileNav.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
-import { Link, animateScroll as scroll, } from 'react-scroll';
+import { Link } from 'react-scroll';
 
 const MobileNav = () => {
   const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
