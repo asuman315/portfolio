827ffaad6ceecedfa92e45331a01067ca01743bc
asumanwebdev <asumanwebdev@gmail.com>
2022-06-13 12:39:30 +0300

    edit contact info and replace resume

--- src/Components/Footer/Contacts.jsx
+++ src/Components/Footer/Contacts.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Contacts = () => {
   return (
     <div name='Contact' className='bg-primary-1 p-4 w-autofill w-[fit-content] rounded-sm mx-auto mt-6 h-[fit-content]'>
-      <div>
+      {/* <div>
         <p>Phone</p>
         <p>+256 785598694</p>
-      </div>
+      </div> */}
       <div className='pt-2'>
         <p>Email</p>
         <p>asumanssendegeya@gmail.com</p>
